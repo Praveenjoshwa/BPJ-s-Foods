d6b09905aaf271ef6e8fb809af890709c85c8705
Praveenjoshwa <bpj4510@gmail.com>
2026-01-07 16:08:45 +0530

    Use env var for DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://bpj4510:bpj4510@cluster0.mhbx0yr.mongodb.net/food-del?appName=Cluster0').then(() => console.log("DB Connected"));
+    await mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => console.log("DB Connected"));
     // await mongoose.connect('mongodb://127.0.0.1:27017/food-del').then(() => console.log("DB Connected"));
 }
